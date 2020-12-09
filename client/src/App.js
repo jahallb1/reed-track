00747fabb9138a37e041d0d319296899a925fff6
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-12-08 19:25:01 -0500

    added nav bar beginnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
       <main>
+        <Nav />
         <About />
       </main>
     </div>
